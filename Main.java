98ac5dc4a30f0763f2e99d43671baa3d05c58d4f
vishal kolhe <kolhe.vishal215@gmail.com>
2024-06-25 00:33:57 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+class Main{
+
+    public static void main(String args[])
+{
+    System.out.println("This is java program...");
+    System.out.println("output from java prgram...");
+    System.out.println(new Date());
+    
+}
+
+}
